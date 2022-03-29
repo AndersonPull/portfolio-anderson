630b08853770a1fa7645a0963028f6ec32a62faf
AndersonPull <andersonoliveera@outlook.com>
2022-03-29 13:57:38 +0000

    Deploying to gh-pages from  @ cd50383c677dac5e9653a5e695a77d92ecf238f5 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -133,7 +133,7 @@
       "url": "_framework\/dotnet.5.0.15.js"
     },
     {
-      "hash": "sha256-9nygGSQ+FuVL1nElCRecdzmHe18JlO7pCgb2YuNZ5Ho=",
+      "hash": "sha256-L26mo4+fwFMllwObTo7JPoOJuudpem7kbj73EeMwQnU=",
       "url": "Portfolio.styles.css"
     },
     {
@@ -197,7 +197,7 @@
       "url": "_framework\/System.IO.Pipelines.dll"
     },
     {
-      "hash": "sha256-8U9TD0eMlux7x8UmA6XaBp+iWBkilLAQXTDKYWe8Dio=",
+      "hash": "sha256-eyQx85Nenz73MpyhAqrV5\/UlyiDhyE3cRQ029akXOpQ=",
       "url": "_framework\/Portfolio.dll"
     },
     {
@@ -269,7 +269,7 @@
       "url": "_framework\/System.Private.CoreLib.dll"
     },
     {
-      "hash": "sha256-WLQsTc5HdMlI9h2jlDxSdnj\/0uA+7RwZBrh6j1TV6Gs=",
+      "hash": "sha256-6caycv2sOInFpN0h4EBRS9amndLdhb9UtlXE9W+toAw=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -277,5 +277,5 @@
       "url": "_framework\/blazor.webassembly.js"
     }
   ],
-  "version": "1F224+ke"
+  "version": "bPRdRCrF"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: 1F224+ke */
+/* Manifest version: bPRdRCrF */
