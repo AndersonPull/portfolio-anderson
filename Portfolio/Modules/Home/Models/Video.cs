58d243b75eca6b52cbc308a441d4182559ebbc0a
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2022-09-20 20:42:24 -0300

    ajustes de modal

---
+++ Portfolio/Modules/Home/Models/Video.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Portfolio.Modules.Home.Models
+{
+    public class Video
+    {
+        public string Src { get; set; }
+    }
+}
---
+++ Portfolio/Shared/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Portfolio.Shared.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Style { get; set; }
+        public string Class { get; set; }
+    }
+}
