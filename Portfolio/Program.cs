59704f22db0bc379bc8c21ba8bb1ad78a59a0af4
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-05-05 18:01:14 -0300

    Update Program.cs

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Blazored.Modal;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Portfolio;
 
-namespace Portfolio
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredModal();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredModal();
+
+await builder.Build().RunAsync();
