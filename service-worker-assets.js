6edabaa05689c095a37a59d155e2c8800be7ca05
AndersonPull <andersonoliveera@outlook.com>
2024-05-12 11:02:13 +0000

    Deploying to gh-pages from  @ 4812bc5eba97e8530542bb1d63e2036fb240980e 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -105,7 +105,7 @@
       "url": "_framework\/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-FzMPbVUEk30WtJskSJSkbhyvFLla6nyeWRGjU3SU\/1s=",
+      "hash": "sha256-k2qviNRFJ3v9I\/A1xnIHsYUbXjPdTCYVKJ9voPBXjIM=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -181,7 +181,7 @@
       "url": "_framework\/Newtonsoft.Json.wasm"
     },
     {
-      "hash": "sha256-wMSZz\/1xiy6Xs\/9t1j2C5bay+K5dmTHJ9QElsjw20TU=",
+      "hash": "sha256-6A9YVXvdGnHfpBqxHSKWmPSiYEck5YGFavcSMoOpTsg=",
       "url": "_framework\/Portfolio.wasm"
     },
     {
@@ -365,5 +365,5 @@
       "url": "_content\/Blazored.Modal\/blazored.modal.js"
     }
   ],
-  "version": "g4qVneYv"
+  "version": "7MLIeIAQ"
 };
