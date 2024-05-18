7fa78da8d295e049d158148fc7d55df0e780ae85
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-05-18 16:21:44 -0300

    ajustes

--- Portfolio/Modules/Newsletter/Models/ArticleModel.cs
+++ Portfolio/Modules/Newsletter/Models/ArticleModel.cs
@@ -1,18 +1,40 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Portfolio.Modules.Newsletter.Models
 {
     public class ArticleModel
     {
+        [JsonProperty("title")]
         public string Title { get; set; }
+
+        [JsonProperty("description")]
         public string Description { get; set; }
-        public DateTime Published_at { get; set; }
+
+        [JsonProperty("published_at")]
+        public DateTime PublishedAt { get; set; }
+
+        [JsonProperty("url")]
         public string Url { get; set; }
-        public string Cover_image { get; set; }
-        public List<string> Tag_list { get; set; }
-        public string Comments_count { get; set; }
-        public string Reading_time_minutes { get; set; }
-        public string Public_reactions_count { get; set; }
+
+        [JsonProperty("cover_image")]
+        public string CoverImage { get; set; }
+
+        [JsonProperty("tag_list")]
+        public List<string> TagList { get; set; }
+
+        [JsonProperty("comments_count")]
+        public string CommentsCount { get; set; }
+
+        [JsonProperty("reading_time_minutes")]
+        public string ReadingTimeMinutes { get; set; }
+
+        [JsonProperty("public_reactions_count")]
+        public string PublicReactionsCount { get; set; }
+
+        [JsonProperty("body_markdown")]
+        public string BodyMarkdown { get; set; }
+        
     }
 }
