975750a1fcd1892fdb9e4a9327f1b42fccfc65ef
Anderson Bezerra <andersonoliveera@outlook.com>
2026-08-08 17:39:04 -0300

    controle de cache e new game

--- Portfolio/wwwroot/service-worker.js
+++ Portfolio/wwwroot/service-worker.js
@@ -1,4 +1,22 @@
-﻿// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+﻿const cachePrefix = 'offline-cache-';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(
+                keys
+                    .filter(key => key.startsWith(cachePrefix))
+                    .map(key => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === self.location.origin) {
+        event.respondWith(fetch(event.request, { cache: 'no-store' }));
+    }
+});
